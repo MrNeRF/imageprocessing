e7a88dfeb71e345cde2c76a6e84edc4bf9e6045e
paja <janusch.patas@gmx.de>
2019-08-25 23:07:51 +0200

    image data and update on Shader class

---
+++ src/Shader.cpp
@@ -0,0 +1,102 @@
+#include "Shader.h"
+#include <GL/glew.h>
+
+
+Shader::Shader(const std::string& vertexShaderPath, const std::string& fragmentShaderPath)
+    : m_vertexShaderPath(vertexShaderPath), m_fragmentShaderPath(fragmentShaderPath)
+{
+    m_shaderTypeMapping[ShaderType::Vertex]     = "VERTEX";
+    m_shaderTypeMapping[ShaderType::Fragment]   = "FRAGMENT";
+    m_shaderTypeMapping[ShaderType::Program]    = "PROGRAM";
+
+    initShaders();
+}
+
+void Shader::initShaders(void)
+{
+    std::string vertexCode;
+    std::string fragmentCode;
+
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        vShaderFile.open(m_vertexShaderPath.c_str());
+        fShaderFile.open(m_fragmentShaderPath.c_str());
+        std::stringstream vShaderStream, fShaderStream;
+
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+
+        vShaderFile.close();
+        fShaderFile.close();
+
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+
+    }
+    catch(const std::ifstream::failure& e)
+    {
+        std::cerr << "ERROR: Shader file not successfully read: " << e.what() << '\n';
+    }
+
+    unsigned int vertexShaderID, fragmentShaderID;
+
+    vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+    const GLchar *vertexShaderCode = vertexCode.c_str();
+    glShaderSource(vertexShaderID, 1, &vertexShaderCode, NULL);
+    glCompileShader(vertexShaderID);
+    checkCompileErrors(vertexShaderID, ShaderType::Vertex);
+
+    fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+    const GLchar *fragmentShaderCode = fragmentCode.c_str();
+    glShaderSource(fragmentShaderID, 1, &fragmentShaderCode, NULL);
+    glCompileShader(fragmentShaderID);
+    checkCompileErrors(fragmentShaderID, ShaderType::Fragment);
+
+    glLinkProgram(m_shaderProgramID);
+    checkCompileErrors(m_shaderProgramID, ShaderType::Program);
+    glDeleteShader(vertexShaderID);
+    glDeleteShader(fragmentShaderID);
+
+}
+
+void Shader::checkCompileErrors(unsigned int shaderID, Shader::ShaderType shaderType)
+{
+    int success;
+    char infoLog[1024];
+    if(shaderType == ShaderType::Program)
+    {
+        glGetProgramiv(shaderID, GL_LINK_STATUS, &success);
+        if(!success)
+        {
+            glGetProgramInfoLog(shaderID, 1024, NULL, infoLog);
+            std::cout   << "ERROR::PROGRAM_LINKING_ERROR of type: "  
+                        << m_shaderTypeMapping[shaderType] << "\n"
+                        << infoLog 
+                        << "\n -- ----------------------------------- --\n";
+        }
+
+    }
+
+    else
+    {
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+        if(!success)
+        {
+            glGetProgramInfoLog(shaderID, 1024, NULL, infoLog);
+            std::cout   << "ERROR::SHADER_COMPILATION_ERROR of type: "  
+                        << m_shaderTypeMapping[shaderType] << "\n"
+                        << infoLog 
+                        << "\n -- ----------------------------------- --\n";
+        }
+    }
+
+}
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,53 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <vector>
+#include <memory>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// Forward declaration callback method for resizing a window
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+int main(int, char **)
+{
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    std::unique_ptr<GLFWwindow> window(glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Rendering with OpenGL", NULL, NULL));
+
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to open GLFW window. \n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window.get());
+    glfwSetFramebufferSizeCallback(window.get(), framebuffer_size_callback);
+
+    std::cout << "Renderer: " << glGetString(GL_RENDERER);
+    std::cout << "OpenGL version supported \n" << glGetString(GL_VERSION) << std::endl;
+    glewExperimental = true;
+
+    while(!glfwWindowShouldClose(window.get()))
+    {
+
+    }
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
